da3cd183dd6eb3608f3d3b520c6174d77c73433e
Linder Hassinger <linderhassinger00@gmail.com>
2018-03-06 09:46:25 -0500

    2 cambio

--- app/src/main/java/pe/edu/tecsup/componentsui/EditTextActivity.java
+++ app/src/main/java/pe/edu/tecsup/componentsui/EditTextActivity.java
@@ -3,13 +3,16 @@ package pe.edu.tecsup.componentsui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditTextActivity extends AppCompatActivity {
 
     private EditText firstName, lastName;
     private TextView displayname;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,22 @@ public class EditTextActivity extends AppCompatActivity {
         firstName = (EditText) findViewById(R.id.firstName);
         lastName = (EditText) findViewById(R.id.lastName);
         displayname = (TextView) findViewById(R.id.displayName);
+         button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EditTextActivity.this, "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void register(View view) {
-        String getFirstName = firstName.getText().toString();
-        String getLastName = lastName.getText().toString();
-        if(getFirstName.length() == 0 || getLastName.length() == 0)
-            displayname.setText("Campos requeridos");
-        else
-            displayname.setText("Tu username es " + getFirstName + " " + getLastName);
+        String firstName = this.firstName.getText().toString();
+        String lastName = this.lastName.getText().toString();
+        if (firstName.length() == 0 || lastName.length() == 0)
+            Toast.makeText(this, "Debe indicar todos los campos", Toast.LENGTH_SHORT).show();
+        return;
     }
+
 }
 
